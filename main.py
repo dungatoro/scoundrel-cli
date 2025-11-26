e075586193dbfb97ee5f0e88f31bf3b48fc3f587
dungatoro <noahjbarker@icloud.com>
2025-11-26 20:10:30 +0000

    added fighting monsters

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 
-MONSTER = "):"
-POTION  = "<3"
-WEAPON  = "/@"
+MONSTER = "£"
+POTION  = "&"
+WEAPON  = "/"
 
 MAX_HEALTH = 20
 
@@ -18,8 +18,8 @@ class Dungeon:
 
     def __init__(self):
         health = MAX_HEALTH
-        equipped_weapon = None
-        weapon_bound = 13 # the weapon can be used on anything
+        weapon = None
+        weapon_bound = 14 # the weapon can be used on anything
 
         room = []
         deck = []
@@ -27,17 +27,26 @@ class Dungeon:
     
     def drink(self, potion_val: int):
         if (potion_val, POTION) in self.room:
-            print(f"Drinking the potion brings your health to {self.health}.")
+            if self.health == 20:
+                print("You drink the potion to no effect. You are at full health.")
+            else:
+                print(f"Drinking the potion restores your health.")
             self.room = [item for item in self.room if item[1] != POTION] 
         else:
-            print("Item not found in this room.")
+            print("No such potion in this room.")
 
     def fight(self, monster_val: int, barehanded: bool):
         if (monster_val, MONSTER) not in self.room:
-            print("No such monster in the room.")
+            print("No such monster in this room.")
         elif barehanded:
             health -= monster_val
-        elif self.weapon_bound < monster_val:
+            self.room.remove((monster_val, MONSTER))
+        elif self.weapon_bound > monster_val:
+            if self.weapon < monster_val:
+                health -= monster_val - self.weapon
+                print("You're weapon stops some of the damage.")
+                self.weapon_bound
+        else:
             print("Your weapon won't work on this monster.")
 
 
